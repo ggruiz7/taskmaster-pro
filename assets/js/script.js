89470c479274eabaf80b6543c87c2637f686262d
Gabriel Ruizz <garciagabriel1945@gmail.com>
2022-01-08 17:39:19 -0800

    used bootstrap componets to adjust page look/layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var tasks = {};
 
+
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
@@ -18,6 +19,7 @@ var createTask = function(taskText, taskDate, taskList) {
   $("#list-" + taskList).append(taskLi);
 };
 
+
 var loadTasks = function() {
   tasks = JSON.parse(localStorage.getItem("tasks"));
 
@@ -31,6 +33,7 @@ var loadTasks = function() {
     };
   }
 
+
   // loop over object properties
   $.each(tasks, function(list, arr) {
     // then loop over sub-array
@@ -40,9 +43,48 @@ var loadTasks = function() {
   });
 };
 
+
 var saveTasks = function() {
   localStorage.setItem('tasks', JSON.stringify(tasks));
 };
+
+
+// modal was triggered
+$('#task-form-modal').on('show.bs.modal', function() {
+  // clear values
+  $("#modalTaskDescription, #modalDueDate").val("");
+});
+
+
+// modal is fully visible
+$("#task-form-modal").on("shown.bs.modal", function() {
+  // highlight textarea
+  $("#modalTaskDescription").trigger("focus");
+});
+
+
+// save button in modal was clicked
+$("#task-form-modal .btn-primary").click(function() {
+  // get form values
+  var taskText = $("#modalTaskDescription").val();
+  var taskDate = $("#modalDueDate").val();
+
+  if (taskText && taskDate) {
+    createTask(taskText, taskDate, "toDo");
+
+    // close modal
+    $("#task-form-modal").modal("hide");
+
+    // save in tasks array
+    tasks.toDo.push({
+      text: taskText,
+      date: taskDate
+    });
+
+    saveTasks();
+  }
+});
+
 
 $('.list-group').on('click', 'p', function() {
   const text = $(this)
@@ -58,6 +100,7 @@ $('.list-group').on('click', 'p', function() {
 
   textInput.trigger('focus');
 });
+
 
 $('.list-group').on('blur', 'textarea', function() {
   // get the textarea's current value/text
@@ -88,6 +131,7 @@ $('.list-group').on('blur', 'textarea', function() {
   $(this).replaceWith(taskP);
 });
 
+
 // due date was clicked
 $('.list-group').on('click', 'span', function() {
   // get current text
@@ -107,6 +151,7 @@ $('.list-group').on('click', 'span', function() {
   // automatically focus on new element
   dateInput.trigger('focus');
 });
+
 
 // value of the due date was changed
 $('.list-group').on('blur', "input[type='text']", function() {
@@ -139,39 +184,6 @@ $('.list-group').on('blur', "input[type='text']", function() {
   $(this).replaceWith(taskSpan);
 })
 
-// modal was triggered
-$('#task-form-modal').on('show.bs.modal', function() {
-  // clear values
-  $("#modalTaskDescription, #modalDueDate").val("");
-});
-
-// modal is fully visible
-$("#task-form-modal").on("shown.bs.modal", function() {
-  // highlight textarea
-  $("#modalTaskDescription").trigger("focus");
-});
-
-// save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
-  // get form values
-  var taskText = $("#modalTaskDescription").val();
-  var taskDate = $("#modalDueDate").val();
-
-  if (taskText && taskDate) {
-    createTask(taskText, taskDate, "toDo");
-
-    // close modal
-    $("#task-form-modal").modal("hide");
-
-    // save in tasks array
-    tasks.toDo.push({
-      text: taskText,
-      date: taskDate
-    });
-
-    saveTasks();
-  }
-});
 
 // remove all tasks
 $("#remove-tasks").on("click", function() {
@@ -181,6 +193,7 @@ $("#remove-tasks").on("click", function() {
   }
   saveTasks();
 });
+
 
 // load tasks for the first time
 loadTasks();
